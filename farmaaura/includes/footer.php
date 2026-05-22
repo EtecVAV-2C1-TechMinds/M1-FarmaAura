d00d014ec7664e52781df4193c406a194c36629f
isafer2511 <isa.fernanda.251109@gmail.com>
2026-05-22 15:40:35 -0300

    Creating the main folder and adding includes (header and footer)

---
+++ farmaaura/includes/footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<footer>
+    <p>&copy; 2026 - Criado com HTML5 e CSS3 por Isabella Fernanda, Larissa Ribeiro e Maria Luisa</p>
+</footer>
---
+++ farmaaura/includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FarmaAura</title>
+    <link rel="stylesheet" href="css/style.css?v=1">
+</head>
+<body>
+
+    <header class="cabeca">
+        <div class="topo">
+            <h1>FarmaAura</h1>
+            <img src="images/logo.png" alt="Logo">
+        </div>
+    </header>
+
+    <nav class="menu">
+        <ul>
+            <li><a href="index.php">Início</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
+        </ul>
+    </nav>
