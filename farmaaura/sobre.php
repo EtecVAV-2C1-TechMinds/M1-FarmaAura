86aa921b8b5e20422fd399e5eaddd64934279366
“isafer2511 <isa.fernanda.251109@gmail.com>
2026-05-22 12:15:15 -0300

    Adição pasta, index e sobre

---
+++ farmaaura/sobre.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<?php 
+    require_once 'includes/header.php'; 
+    require_once 'conexao.php';
+?>
+<link rel="stylesheet" href="css/style.css?v=1">
+</head>
+
+<body>
+    <main>
+        <h1>Sobre</h1>
+        <div class="text">Fundada em 2026, a FarmaAura nasceu com uma missão simples: descomplicar o cuidado com a sua saúde e bem-estar.
+Acreditamos que a verdadeira saúde vai além do essencial — ela reflete na nossa energia e no nosso equilíbrio diário. Por isso, combinamos modernidade, atendimento humano e agilidade para fornecer o suporte necessário em momentos de saúde delicados.
+A nossa missão é a conexão do paciente às melhores soluções de saúde e autocuidado de forma rápida e segura e o nosso compromisso é de promover transparência e cuidado a todos.
+<br><br><img class="fotosobre" src="interior.png" alt="Logo">
+</div>
+
+    </main>
+
+     <?php require_once 'includes/footer.php'; ?>
+
+</body>
+</html>
